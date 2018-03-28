6eb31276af9585a44a53627e788a18366a19b97a
Unknown <rasmus.rolfsen@mail.dk>
2018-03-28 13:18:37 +0200

    Water Usage Function

--- Assets/scripts/EventHandler/Event Types/GamePlayEvent.cs
+++ Assets/scripts/EventHandler/Event Types/GamePlayEvent.cs
@@ -31,9 +31,22 @@ public abstract class GamePlayEvent : BaseEvent
 		}
 	}
 
-
 	public class TestEvent3: GamePlayEvent
 	{
 
 	}
+
+	public class TaskComplete : GamePlayEvent
+	{
+		public TaskComplete(int taskNumber, float waterUsed, string taskName)
+		{
+			this.taskNumber = taskNumber;
+			this.waterUsed = waterUsed;
+			this.taskName = taskName;
+		}
+
+		public int taskNumber { get; private set; }
+		public float waterUsed { get; private set; }
+		public string taskName { get; private set; }
+	} 
 }
--- Assets/scripts/WaterUsage.cs
+++ Assets/scripts/WaterUsage.cs
@@ -2,18 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+struct WaterTask
+{
+	public string taskName;
+	public List<float> waterUsage;
+}
+
+
 public class WaterUsage : MonoBehaviour {
 
-	[SerializeField]List<float> waterSpendForTask;
-	[SerializeField]float totalWaterSpend;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	[SerializeField]float totalWaterSpend;
+	[SerializeField] List<WaterTask> WaterUsingTasks;
+
+	private void Awake()
+	{
+		EventBus.AddListener<GamePlayEvent.TaskComplete>(UpdateWaterUsage);
+	}
+
+	private void UpdateWaterUsage (object sender, GamePlayEvent.TaskComplete e)
+	{
+		WaterUsingTasks[e.taskNumber].waterUsage.Add(e.waterUsed);
+		WaterUsingTasks[e.taskNumber].waterUsage.Sort();
+
+		totalWaterSpend = 0;
+
+		foreach (var task in WaterUsingTasks)
+		{
+			if (task.waterUsage.Count != 0)
+			{
+				totalWaterSpend += task.waterUsage[0];
+			}
+		}
 	}
 }
