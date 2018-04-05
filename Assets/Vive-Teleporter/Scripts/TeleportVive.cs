defa3b42d964dafb3750e40c2da09a10609cba0f
Unknown <rasmus.rolfsen@mail.dk>
2018-04-05 11:58:00 +0200

    Some debug log and the script kiril wanted

--- Assets/Vive-Teleporter/Scripts/TeleportVive.cs
+++ Assets/Vive-Teleporter/Scripts/TeleportVive.cs
@@ -199,6 +199,7 @@ public class TeleportVive : MonoBehaviour {
                 // If the user has decided to cancel (ie squeezes grip button) then remove visual indicators and do nothing
                 if (shouldTeleport && Pointer.PointOnNavMesh)
                 {
+					Debug.Log("Alter if statement so it depends also depend on Raycast tag");
                     // Begin teleport sequence
                     CurrentTeleportState = TeleportState.Teleporting;
                     TeleportTimeMarker = Time.time;
---
+++ Assets/scripts/BoxTeleport.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxTeleport : MonoBehaviour
+{
+
+	[SerializeField]
+	float raycastDist;
+
+	[SerializeField]
+	float angle;
+
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(0, angle * Time.deltaTime, 0, Space.World);
+		}
+
+		Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward)*raycastDist);
+
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+			Ray ray;
+			RaycastHit hit;
+
+			float raycastAngle = angle;
+
+			ray = new Ray(transform.position, transform.TransformDirection(Vector3.forward) * raycastDist);
+
+			if (Physics.Raycast(ray, out hit, raycastDist))
+			{
+				transform.position = hit.point;
+				
+			}
+		}
+	}
+}
