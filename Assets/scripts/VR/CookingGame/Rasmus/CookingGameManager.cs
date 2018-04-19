038000bb1a4dd9210475b00ee495f1f56ea2d6de
Unknown <rasmus.rolfsen@mail.dk>
2018-04-19 14:06:18 +0200

    Incomplete potato code

---
+++ Assets/scripts/VR/CookingGame/Rasmus/CookingGameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookingGameManager : MonoBehaviour {
+
+	[SerializeField]
+	int numberOfPotatoes;
+
+	int readyPotatoes;
+
+	private void Awake()
+	{
+		EventBus.AddListener<GameStateEvent.PotatoComplete>(PotatoComplete);
+	}
+
+	private void PotatoComplete (object sender, GameStateEvent.PotatoComplete e)
+	{
+		readyPotatoes++;
+
+		if (numberOfPotatoes == readyPotatoes)
+		{
+			EventBus.TriggerEvent(this, new GameStateEvent.AllPotatoesComplete());
+		}
+	}
+}
