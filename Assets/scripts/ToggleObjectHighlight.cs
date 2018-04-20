68da6424cb71da649eaf1fc9421e4e42c3a0c8f3
Unknown <rasmus.rolfsen@mail.dk>
2018-04-20 17:26:43 +0200

    Code for adjusting intensity updated

--- Assets/scripts/ToggleObjectHighlight.cs
+++ Assets/scripts/ToggleObjectHighlight.cs
@@ -7,32 +7,53 @@ public class ToggleObjectHighlight : MonoBehaviour {
 	[SerializeField]
 	string shaderName;
 
-	[SerializeField,Tooltip("Name of shader property")]
-	string propName;
+	[SerializeField, Tooltip("Float Name")]
+	string floatName;
+
+	[SerializeField]
+	float pingVal;
+
+	[SerializeField]
+	float slowVal;
+
+	[SerializeField]
+	float minVal;
+
+
 
 	Renderer rend;
-	bool turnOnHightLight;
+	float startVal;
 
 	private void Start()
 	{
 		rend = GetComponent<Renderer>();
-		rend.material.shader = Shader.Find(shaderName);
-		turnOnHightLight = false;
-		// 		
+		rend.material.shader = Shader.Find("DCC/Silhouette Enhancement");
+		//startVal = rend.material.GetFloat(floatName);
+		
+	}
+
+	private void Update()
+	{
+		//DEBUG
+		float adjustFloat = Mathf.PingPong(Time.time / slowVal, pingVal - minVal) + minVal;
+		print(adjustFloat);
+		rend.material.SetFloat("adwad", adjustFloat);
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.CompareTag("Player"))
 		{
-			rend.material.SetInt(propName, 1);
+			float adjustFloat = Mathf.PingPong(Time.time / slowVal, pingVal - minVal) + minVal;
+			rend.material.SetFloat(floatName, adjustFloat);
 		}
 	}
 	private void OnTriggerExit(Collider other)
 	{
 		if (other.CompareTag("Player"))
 		{
-			rend.material.SetInt(propName, 0);
+			rend.material.SetFloat(floatName, startVal);
 		}
+
 	}
 }
