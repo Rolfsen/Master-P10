8f49e7e179bb8c5d73491064433282a311362cc8
Unknown <rasmus.rolfsen@mail.dk>
2018-04-25 14:44:25 +0200

    pushing initial scripts

---
+++ Assets/TestingGrippingHand.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingGrippingHand : MonoBehaviour {
+
+	public bool isLeftHand;
+	public bool isHoldingObject;
+
+	[SerializeField]
+	Mesh holding;
+
+	[SerializeField]
+	Mesh notHolding;
+
+	MeshFilter myMesh;
+
+	private void Start()
+	{
+		myMesh = GetComponent<MeshFilter>();
+	}
+
+	public void HoldObject ()
+	{
+		myMesh.mesh = holding;
+	}
+
+	public void StopHoldindObject()
+	{
+		myMesh.mesh = notHolding;
+	}
+}
---
+++ Assets/TestingGrippingScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingGrippingScript : MonoBehaviour
+{
+
+	[SerializeField]
+	Transform leftHandGrip;
+
+	[SerializeField]
+	Transform rightHandGrip;
+
+	Vector3 offsetVector;
+
+	bool isHold;
+
+	[SerializeField]
+	Transform leftHand;
+
+	[SerializeField]
+	Transform rightHand;
+
+	Transform usingTrans;
+
+	Vector3 offset;
+
+	GameObject holdingHand;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (!isHold && other.CompareTag("Player"))
+		{
+			if (other.GetComponent<TestingGrippingHand>().isLeftHand)
+			{
+				offsetVector = leftHandGrip.position;
+				usingTrans = leftHand;
+			}
+			else
+			{
+				offsetVector = rightHandGrip.position;
+				usingTrans = rightHand;
+			}
+
+			offset = (usingTrans.position - other.transform.position) + (offsetVector - transform.position);
+
+			transform.position = other.transform.position - offset;
+
+			isHold = true;
+			holdingHand = other.gameObject;
+			transform.SetParent(holdingHand.transform);
+			other.GetComponent<TestingGrippingHand>().HoldObject();
+		}
+
+		else if (isHold && other.CompareTag("Flavor"))
+		{
+
+		}
+	}
+}
