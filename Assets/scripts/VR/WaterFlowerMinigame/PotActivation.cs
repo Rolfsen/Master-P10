a3dbfe72173d61aa5f8146e78b997db2c5ecfabf
DragonKing <dragonking@mail.dk>
2018-05-03 14:14:58 +0200

    Pot on and off when the game starts.
    Needs to be put on an object that is not the pot.

--- Assets/scripts/VR/WaterFlowerMinigame/PotActivation.cs
+++ Assets/scripts/VR/WaterFlowerMinigame/PotActivation.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PotActivation : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+    [SerializeField]
+    GameObject pot;
+	void Awake () {
+        pot.SetActive(false);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    
+    void Update()
+    {
+        if (MiniGameManager.isFlowerWateringGameRunning)
+        {
+            pot.SetActive(true);
+        }
+    }
 }
