cf964aaba878ab59b3121a3c7bac5280cd2d8391
Unknown <rasmus.rolfsen@mail.dk>
2018-05-06 17:43:23 +0200

    Class for disable and enable objects.

---
+++ Assets/ActivateGameobject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateGameobject : MonoBehaviour {
+
+	[SerializeField]
+	List<GameObject> targetGameobjects;
+
+	public void DisableTarget(int index)
+	{
+		targetGameobjects[index].SetActive(false);
+	}
+	public void EnableTarget(int index)
+	{
+		targetGameobjects[index].SetActive(true);
+	}
+}
